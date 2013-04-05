e2800a02f210523239dabfbf934cf5449c47fe5d
John Allwine <jallwine86@gmail.com>
2013-04-05 10:11:29 -0600

    added xmllint functionality

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-      response.send('Hello World!');
+    response.render('index', {
+        title: "Emscripten Test"
+    });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-      console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
